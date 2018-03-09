2006ef1ed7a67364a6b9265235bfe96ed95d531f
huliyou <liyoupie@gmail.com>
2018-03-09 18:03:32 +0800

    我的 基础信息

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,6 @@ const app = getApp()
 Page({
   data: {
     selectedTab: 0,
-    clientHeight: 0,
     emergencyList: [
 
     ],
@@ -35,14 +34,8 @@ Page({
     });
   },
   onLoad: function () {
-    wx.getSystemInfo({
-      success: (res) => {
-        this.setData({
-          clientHeight: res.windowHeight
-        });
-      }
-    });
     if (app.globalData.userInfo) {
+      console.log(app.globalData.userInfo);
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
@@ -51,16 +44,18 @@ Page({
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
+        console.log(app.globalData.userInfo);
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
-        })
+        });
       }
     } else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
         success: res => {
           app.globalData.userInfo = res.userInfo
+          console.log(app.globalData.userInfo);
           this.setData({
             userInfo: res.userInfo,
             hasUserInfo: true
