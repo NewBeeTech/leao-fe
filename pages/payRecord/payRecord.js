ece05f944788a6dfa08e47e170dd870afc233578
huliyou <liyoupie@gmail.com>
2018-03-23 20:04:03 +0800

    交易记录 购买课时

---
+++ pages/payRecord/payRecord.js
@@ -0,0 +1,46 @@
+const app = getApp();
+
+Page({
+  date: {
+    token: '',
+    records: [],
+  },
+  onLoad() {
+    // 设置token
+    if (!this.data.token) {
+      this.setData({
+        token: wx.getStorageSync('token'),
+      });
+    }
+    this.getPayRecord();
+  },
+  renderDate(date) {
+    const dateObj = new Date(date);
+    const month = dateObj.getMonth() + 1;
+    const day = dateObj.getDate();
+    const hour = dateObj.getHours();
+    const min = dateObj.getMinutes();
+    return `${month}月${day}日 ${hour}:${min}`;
+  },
+  getPayRecord() {
+    const that = this;
+    wx.showLoading()
+    wx.request({
+      url: 'https://ssl.newbeestudio.com/api/payRecord/my',
+      header: {
+        token: this.data.token,
+      },
+      success: (res) => {
+        wx.hideLoading();
+        if (res.data.code === '000') {
+          let result = res.data.datas;
+          result = result && result.map(item => ({ ...item, createTime: that.renderDate(item.createTime)}));
+          console.log(result);
+          this.setData({
+            records: result,
+          });
+        }
+      }
+    });
+  }
+})
