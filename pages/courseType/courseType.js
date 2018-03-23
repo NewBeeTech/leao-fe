cedd8392bbcc93869a05a29c73bb05664cecc74a
huliyou <liyoupie@gmail.com>
2018-03-23 21:29:26 +0800

    课程类型详情

--- pages/courseType/courseType.js
+++ pages/courseType/courseType.js
@@ -1,12 +1,38 @@
 const app = getApp();
 Page({
+  data: {
+    courseType: [],
+    courseList: [],
+  },
   onLoad(options) {
     const id = options.id
     wx.request({
-      url: 'https://ssl.newbeestudio.com/api/coach/info?typeId='+id,
+      url: 'https://ssl.newbeestudio.com/api/courseType/info?typeId='+id,
       success: res => {
         console.log(res.data);
+        let courseList = res.data.datas.courseList;
+        courseList = courseList.map(course => {
+          const beginTime = new Date(course.beginTime);
+          const min = beginTime.getMinutes() || '00';
+          const hour = beginTime.getHours() > 10 ? beginTime.getHours() : '0'+beginTime.getHours();
+          const month = beginTime.getMonth() + 1;
+          const day = beginTime.getDate();
+          return ({
+            ...course,
+            time: `${hour}:${min}`,
+            date: `${month}月${day}日`,
+          });
+        });
+        this.setData({
+          courseType: res.data.datas.courseType,
+          courseList: courseList,
+        })
       },
+    });
+  },
+  topNavAction: (e) => {
+    wx.reLaunch({
+      url: `/pages/index/index?selectedTab=${e.currentTarget.dataset.selectedtab}`
     });
   },
 });
