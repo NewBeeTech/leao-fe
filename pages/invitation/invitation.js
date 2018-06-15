dda7e697afcf2614724192a646cdf747fb61ec13
deerw <uniqueway@deerwsMacBook-Pro.local>
2018-06-15 10:48:55 +0800

    邀请页面

--- pages/invitation/invitation.js
+++ pages/invitation/invitation.js
@@ -16,9 +16,119 @@ Page({
     this.setData({
       userId
     });
+    this.getToken(
+      (token) => {
+        // 获取用户信息
+        this.getUserInfo();
+      }
+    );
+  },
+  getToken(successCallBack, failureCallBack) {
+    let token = this.data.token || wx.getStorageSync('token');
+    if(token) {
+      this.setData({
+        token,
+      });
+      successCallBack && successCallBack(token);
+      return;
+    }
+    wx.login({
+      success: (res) => {
+        if (res.code) {
+          wx.request({
+            url: 'https://ssl.newbeestudio.com/api/wx/login', //仅为示例，并非真实的接口地址
+            data: {
+              jsCode: res.code,
+            },
+            success: (res) => {
+              if (res.data.code == '000' || res.data.code == '014') { // 获取token成功
+                wx.setStorageSync('token', res.data.datas); // 持久化token
+                this.setData({
+                  token: res.data.datas,
+                });
+                successCallBack && successCallBack(res.data.datas); // 成功回调
+              }
+              if (res.data.code == '014') { // 之前没有使用过运用
+                this.authSaveUserInfo(res.data.datas);
+              }
+            }
+          });
+        } else {
+          failureCallBack && failureCallBack();
+        }
+      }
+    });
+  },
+  authSaveUserInfo(token) {
+    const that = this;
+    wx.getSetting({
+      success: res => {
+        // if (res.authSetting['scope.userInfo']) {
+        if (res.errMsg === 'getSetting:ok') {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: respond => {
+              // this.globalData.userInfo = res.userInfo;
+              wx.request({
+                url: 'https://ssl.newbeestudio.com/api/user/edit', //仅为示例，并非真实的接口地址
+                data: {
+                  nickName: respond.userInfo.nickName,
+                  gender: respond.userInfo.gender,
+                  portrait: respond.userInfo.avatarUrl,
+                },
+                header: {
+                  'content-type': 'application/x-www-form-urlencoded', // 默认值
+                  'token': token,
+                },
+                method: 'POST',
+                success: function(res) {
+                  if (res.data.code == '000') { // 保存用户信息
+                    that.setData({
+                      userInfo: {
+                        nickName: respond.userInfo.nickName,
+                        gender: respond.userInfo.gender,
+                        portrait: respond.userInfo.avatarUrl,
+                      }
+                    });
+                  }
+                }
+              });
+            },
+            fail: res => {
+              console.log(res);
+            }
+          });
+        }
+      }
+    })
+  },
+  getUserInfo() {
+    const that = this;
+    wx.request({
+      url: 'https://ssl.newbeestudio.com/api/user/userinfo', //仅为示例，并非真实的接口地址
+      header: {
+        'token': this.data.token,
+      },
+      method: 'GET',
+      success: function(res) {
+        console.log('getUserInfo===>', res);
+        if (res.data.code == '000') {
+          if (res.data.datas.user) {
+            that.setData({
+              userInfo: res.data.datas.user || {},
+            });
+            app.globalData.userInfo = res.data.datas.user;
+            that.getShareListAction(that.data.selectedBar);
+          } else {
+            that.authSaveUserInfo(that.data.token);
+          }
+
+        }
+      }
+    })
   },
   onShow () {
-    this.getShareListAction(this.data.selectedBar);
+    // this.getShareListAction(this.data.selectedBar);
   },
   inputChangeAction(e) {
     const value = e.detail.value;
@@ -36,10 +146,12 @@ Page({
     // });
     const that = this;
     let token = app.globalData.token || wx.getStorageSync('token');
+    console.log(token);
+    console.log(that.data.userId || (that.data.userInfo ? that.data.userInfo.id : ''));
     wx.request({
       url: 'https://ssl.newbeestudio.com/api/course/moneyRecord', //仅为示例，并非真实的接口地址
       data: {
-        userId:that.data.userId,
+        userId:that.data.userId || (that.data.userInfo ? that.data.userInfo.id : ''),
         type
       },
       header: {
